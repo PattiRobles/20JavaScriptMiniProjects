f68468cbf7acb30a5f5d00cce3958672b2622445
maldonsalt <103450360+maldonsalt@users.noreply.github.com>
2023-05-11 11:03:02 +0100

    select seats and movie, update totals

--- MovieSeatBooking/script.js
+++ MovieSeatBooking/script.js
@@ -7,14 +7,29 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie')
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 //change the selected seatc appearance 2 possible ways:
 //1 - for each to go through elements of the node list that 'seats' is and then add an event listener to each seat OR
 //2 - add an event listener to the container and then make sure that we are clicking in an available seat. This is more efficient
 
+function updateSelecetedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected')
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//selection of movie - change event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value
+})
+
+//selection of seats
 container.addEventListener('click', (e) => {
   if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
    e.target.classList.toggle('selected')
   }
+  updateSelecetedCount();
 })
+
